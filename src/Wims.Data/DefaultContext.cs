68fbc4f37597da79488483ea0f596b3c146c6e0f
Manvir Singh <manvirsingh@kilvikhtech.com>
2019-02-13 12:10:45 +0000

    Updated projects 2.2 version Added dbcontext config.

---
+++ src/Wims.Data/DefaultContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wims.Data.Models;
+
+namespace Wims.Data
+{
+    public class DefaultContext : DbContext
+    {
+        public DbSet<AddressDb> Addresses { get; set; }
+        public DbSet<EventDb> Events { get; set; }
+        public DbSet<LocationDb> Locations { get; set; }
+
+        public DefaultContext(DbContextOptions<DefaultContext> options)
+            : base(options) { }
+    }
+}
--- src/Wims.WebApi/Startup.cs
+++ src/Wims.WebApi/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Wims.Data;
 
 namespace WebApi
 {
@@ -26,6 +28,7 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<DefaultContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Wims")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
